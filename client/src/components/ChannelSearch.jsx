92c9b087b34554f4d512d973405a1259669054df
Aditya Ballaki <77788601+adi-bal@users.noreply.github.com>
2022-09-23 22:55:34 +0800

    update channelSearch to implement searching

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,10 +1,40 @@
 import React, {useState, useEffect} from 'react';
-import { useChatContext } from 'stream-chat-react';
+import { getChannel, useChatContext } from 'stream-chat-react';
+import {SearchIcon} from '../assets';
 
 const ChannelSearch = () => {
-  return (
-    <div>
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState('');
 
+    const getChannels = async (text) => {
+        try {
+            // fetch comments here 
+        } catch (error) {
+            setQuery('');
+        }
+    }
+
+    const onSearch = (event) => {
+        event.preventDefault();
+
+        setLoading(true);
+        setQuery(event.target.value);
+        getChannels(event.target.value);
+    } 
+  return (
+    <div className='channel-search__container'>
+        <div className='channel-search__input__wrapper'>
+            <div className='channel-search__input__icone'>
+                <SearchIcon/>
+            </div>
+            <input 
+                className="channel-search__input__text" 
+                placeholder="search" 
+                type="text" 
+                value={query}
+                onChange={onSearch}
+            />
+        </div>
     </div>
   )
 }
