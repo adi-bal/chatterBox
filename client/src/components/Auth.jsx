a59d4298a0937e496122ed2bd4c452953e4dcbe2
Aditya Ballaki <77788601+adi-bal@users.noreply.github.com>
2022-09-26 02:24:49 +0800

    add new field for authentication page

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -8,24 +8,29 @@ const Auth = () => {
     const [isSignup, setIsSignup] = useState(!false);
 
     const handleChange = () => {};
-  return (
-    <div className='auth__form-container'>
-        <div className='auth__form-container_fields'>
-            <div className='auth__form-container_fields-content'>
-                <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
-                <form onSubmit = {()=>{}}>
-                    {isSignup && (
-                        <div className="auth__form-container_fields-content_input">
-                            <label htmlFor="fullName">Full Name</label>
-                            <input 
-                                name="fullName"
-                                type="text"
-                                placeholder="Full Name"
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    )}
+
+    const switchMode = () =>{
+        setIsSignup(prev => !prev);
+    }
+
+    return (
+        <div className='auth__form-container'>
+            <div className='auth__form-container_fields'>
+                <div className='auth__form-container_fields-content'>
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit = {()=>{}}>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input 
+                                    name="fullName"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
                         <div className="auth__form-container_fields-content_input">
                             <label htmlFor="username">username</label>
                             <input 
@@ -36,11 +41,57 @@ const Auth = () => {
                                 required
                             />
                         </div>
-                </form>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="phoneNumber">Full Name</label>
+                                <input 
+                                    name="phoneNumber"
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="avatarURL">Avatar URL</label>
+                                <input 
+                                    name="avatarURL"
+                                    type="text"
+                                    placeholder="Avatar URL"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input 
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="confirm Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Auth
