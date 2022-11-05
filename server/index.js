a12d53e9289cdedba5bf2a0746842452d0e3ebe6
Aditya Ballaki <77788601+adi-bal@users.noreply.github.com>
2022-11-05 18:26:42 +0800

    Create server on port 5000

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express'); 
+const cors = require('cors');
+
+
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(PORT, () => console.log(`Server is runnning at ${PORT}`));
