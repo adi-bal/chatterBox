5aaea1ae126fd000137e3964c3a3fbc3dba00177
Aditya Ballaki <77788601+adi-bal@users.noreply.github.com>
2022-11-19 19:47:38 +0800

    Update logic to auth controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,20 @@
+const {connect} = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat');
+const crypto = require('crypto');
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const api_id = process.env.STREAM_APP_ID;
+
 const signup = (req, res) => {
     try {
         const {fullNume, username, password, phoneNumber} = req.body;
+
+        const userId = crypto.randomBytes(16).toString('hex');
+        const serverClient = connect(api_key, api_secret, api_id);
+
+
     } catch (error) {
         console.log(error);
 
